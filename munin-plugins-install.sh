952cb5a87f18fd059e20fb430e7619dced22a22c
Anthony Scalisi <scalisi.a@gmail.com>
2012-06-03 00:17:00 -0700

    draft automation plugins install

---
+++ munin-plugins-install.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+## munin-plugins-install.sh for plugins install in /Users/fresh/nagios/munin-node-smartos
+##
+## Made by Anthony Scalisi
+## <scalisi.a@gmail.com>
+##
+## Started on  Sun Jun  3 00:16:24 2012 Anthony Scalisi
+## Last update Sun Jun  3 00:16:30 2012 Anthony Scalisi
+##
+
+host=`hostname | awk -F '.' '{print $1}'`
+ip=`ifconfig -a | grep 'inet 192' | awk '{print $2}'`
+
+cd /usr/local/munin/lib &&
+
+wget --no-check-certificate https://github.com/scalp42/munin-node-smartos/raw/master/plugins.tar.gz
+
+tar xzvf plugins.tar.gz
+
+chown -R munin:munin /usr/local/munin/lib/plugins
+
+/usr/local/munin/sbin/munin-node-configure --shell --families=contrib,auto | sh -x
+
+rm -f plugins.tar.gz*
+
+rm -f ~/munin-plugins-install.sh*
